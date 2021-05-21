a08d2a3cc306825908a22a591071d401452297a4
VulpesPaw <richard.jh.johansson@gmail.com>
2021-05-21 14:25:04 +0200

    Jwt Token Verification

--- auth.js
+++ auth.js
@@ -1,7 +1,18 @@
+/* Middleware for token verification
+----- */
+// Keeps users logged in
 const jwt = require('jsonwebtoken');
-module.exports = () => {
-  /*
-TODO: Fill with auth
 
-    */
+module.exports = (req, res, next) => {
+  let tokenFromUser = req.headers.token;
+
+  try {
+    let userToken = req.query.token;
+
+    let check = jwt.verify(userToken, tokenFromUser);
+
+    next();
+  } catch (err) {
+    res.status(401).send({ error: err.message });
+  }
 };
