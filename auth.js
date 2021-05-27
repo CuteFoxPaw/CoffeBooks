b15b43f24dc05d40052ae4edcc1d3d0f44afad27
VulpesPaw <richard.jh.johansson@gmail.com>
2021-05-27 19:09:33 +0200

    JWT / Auth Fixed, WORKING

--- auth.js
+++ auth.js
@@ -2,17 +2,62 @@
 ----- */
 // Keeps users logged in
 const jwt = require('jsonwebtoken');
+//const cookieParser = require('cookie-parser');
 
 module.exports = (req, res, next) => {
-  let tokenFromUser = req.headers.token;
-
+  /*
   try {
-    let userToken = req.query.token;
+    const tokenFromUser = req.cookies.token;
+    console.log(req.cookies);
 
-    userToken = jwt.verify(userToken, tokenFromUser);
-    req.userId = userToken.id;
+    //if (tokenFromUser == null || tokenFromUser == typeof undefined)
+    // return res.sendStatus(401);
 
-    next();
+    // let userToken = req.query.token;
+    console.log(`🚀 ~ file: auth.js ~ line 12 ~ userToken`, userToken);
+
+    userToken = jwt.verify(
+      tokenFromUser,
+      process.env.ACCESS_TOKEN_SECRET,
+      (err, id) => {
+        if (err) return res.sendStatus(403);
+        req.user = id;
+        next();
+      }
+    );
+    // req.userId = userToken.id;s
+
+    //next();
+  } catch (err) {
+    res.status(401).send({ error: err.message });
+  }*/ try {
+    let tokenFromUser = req.cookies['token'];
+
+    //if (token == null || token == typeof undefined)
+    // return res.sendStatus(401);
+
+    // let userToken = req.query.token;
+    // console.log(`🚀 ~ file: auth.js ~ line 12 ~ userToken`, userToken);
+
+    tokenFromUser = jwt.verify(
+      tokenFromUser,
+      process.env.ACCESS_TOKEN_SECRET,
+      (err, id) => {
+        if (err) {
+          console.log('Error FIERD!');
+
+          return res.sendStatus(403);
+        }
+        console.log('Asignment');
+        req.userid = tokenFromUser.id;
+        console.log('Asignment');
+
+        next();
+      }
+    );
+    // req.userId = userToken.id;
+
+    //next();
   } catch (err) {
     res.status(401).send({ error: err.message });
   }
